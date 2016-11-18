e2899a30d8a3a28d583884b44126475792b098e5
oldcodeoberyn <lex.lee327@gmail.com>
2016-11-18 14:00:27 +0800

    first commit

---
+++ src/main/java/com/example/HibernateMongodbPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateMongodbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateMongodbPracticeApplication.class, args);
+	}
+}
